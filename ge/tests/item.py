377e4442953ae655698ebb7c9975195d894797f0
Yannick <yannick.dorn@gmail.com>
2021-12-24 13:20:11 -0600

    reformatted tools a bit, added some tests. Don't understand imports

--- ge/tests/item.py
+++ ge/tests/item.py
@@ -1,4 +1,7 @@
 from datetime import date
+import .structs
+import tools
+from osrsbox import items_api
 ITEM_DATA = {
 	'id': 2611,
 	'name': "Abyssal Whip",
@@ -13,3 +16,7 @@ ITEM_DATA = {
 		}
 	}
 }
+def get_none_id() -> bool:
+	item_list = items_api.load()
+	# tools.get_id(item_list, 5000000)
+	assert(None, tools.get_id(item_list, "5000000"))
