3b0c26e336ecaee3a4de0948a683c0b0dab431f9
Yannick <BrokeBoat@protonmail.com>
2022-06-16 12:19:28 -0400

    refactored endpoints

--- ge/endpoints.py
+++ ge/endpoints.py
@@ -1,32 +1,21 @@
 from enum import Enum
 import requests as req
-
-header = {
-	'User-agent': 'github.com/2mill/osrs_exchange'
-}
 base_url: str = "https://prices.runescape.wiki/api/v1/osrs"
 class Timestep(Enum):
 	FIVE_MIN="5m"
 	ONE_HOUR="1h"
 	SIX_HOURS="6h"
+class Timestamp(Enum):
+	FIVE_MIN="5m"
+	ONE_HOUR="1h"
 
+def make_req(endpoint_url: str) -> req.Response:
+	header = {
+		'User-agent': 'github.com/2mill/osrs_exchange'
+	}
+	return req.get(endpoint_url, header)
 
-def get_latest(id:int) -> object:
-	endpoint = f"{base_url}/latest?id={id}"
-	return req.get(endpoint, headers=header)
-
-def get_latest_all() -> object:
-	endpoint = f"{base_url}/latest"
-	return req.get(endpoint, headers=header)
-
-
-def get_mapping() -> object:
-	endpoint = f"{base_url}/mapping"
-	return req.get(endpoint, headers=header)
-def get_timestep(timestep: Timestep, id: int) -> object:
-	endpoint = f"{base_url}/timeseries?timestep={timestep.value}&id={id}"
-	return req.get(endpoint, headers=header)
-
-
-
-
+latest_all = lambda: make_req(f"{base_url}/latest?")
+mapping = lambda: make_req(f"{base_url}/mapping?")
+timestamp = lambda time: make_req(f"{base_url}/{time.value}")
+timeseries = lambda id, timestep: make_req(f"{base_url}/timeseries?id={id}&timestep={timestep.value}")
