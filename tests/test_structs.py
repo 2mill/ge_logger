f8cc2516a00c1e1c9e2137a418012cbea224422c
Yannick <BrokeBoat@protonmail.com>
2022-06-19 17:08:02 -0400

    cleaned up and formatted

--- tests/test_structs.py
+++ tests/test_structs.py
@@ -1,49 +1,47 @@
-<<<<<<< HEAD
 from os import times_result
 from numpy import identity
 import ge.structs as structs
-=======
 from wikige import structs
->>>>>>> main
 import json
 
 
 # Not trying to accidentally DDOS the wiki here.
-mapping_data = json.load(open('./tests/files_for_testing/mapping.json', 'r'))
+mapping_data = json.load(open("./tests/files_for_testing/mapping.json", "r"))
 
 total_items = 0
 for item in mapping_data:
-	total_items = total_items + 1
+    total_items = total_items + 1
 
-lookup_all_data = json.load(open('./tests/files_for_testing/latest.json', 'r'))
-abyssal_whip_lookup = json.load(open('./tests/files_for_testing/latest_aby_whip.json', 'r'))
-timeseries = json.load(open('./tests/files_for_testing/timeseries.json', 'r'))
-timestamp = json.load(open('./tests/files_for_testing/timestamp.json'))
-mapping_data = json.load(open('./tests/files_for_testing/mapping.json'))
+lookup_all_data = json.load(open("./tests/files_for_testing/latest.json", "r"))
+abyssal_whip_lookup = json.load(
+    open("./tests/files_for_testing/latest_aby_whip.json", "r")
+)
+timeseries = json.load(open("./tests/files_for_testing/timeseries.json", "r"))
+timestamp = json.load(open("./tests/files_for_testing/timestamp.json"))
+mapping_data = json.load(open("./tests/files_for_testing/mapping.json"))
+
+
 def test_item_list():
-	item_list = structs.ItemList(mapping_data)
-	# assert len(item_list.find(4151)) == 1
-	assert len(item_list.item_list) == len(mapping_data)
+    item_list = structs.ItemList(mapping_data)
+    # assert len(item_list.find(4151)) == 1
+    assert len(item_list.item_list) == len(mapping_data)
+
+
 def test_itempricinginformation():
-	item = structs.ItemPricingInformation(4151, abyssal_whip_lookup['data']['4151'])
-	assert item.id == 4151
+    item = structs.ItemPricingInformation(4151, abyssal_whip_lookup["data"]["4151"])
+    assert item.id == 4151
+
+
 def test_timedpricinginformation_timestamp():
-	timed_item = structs.TimedItemPricingInformation(
-		4151,
-		timestamp['data']['4151'],
-		timestamp['timestamp']
-	)
-	assert timed_item.id == 4151
+    timed_item = structs.TimedItemPricingInformation(
+        4151, timestamp["data"]["4151"], timestamp["timestamp"]
+    )
+    assert timed_item.id == 4151
+
+
 def test_timedpricinginformation_timeseries():
-	data_timestamp = timeseries['data'][0]['timestamp']
-	timed_item = structs.TimedItemPricingInformation(
-		4151,
-		timeseries['data'][0],
-		data_timestamp
-	)
-	assert timed_item.id == 4151
-
-
-
-
-
+    data_timestamp = timeseries["data"][0]["timestamp"]
+    timed_item = structs.TimedItemPricingInformation(
+        4151, timeseries["data"][0], data_timestamp
+    )
+    assert timed_item.id == 4151
